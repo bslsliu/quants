7d27582b94a279925d00d7a1a5f2ac87caa74703
liubsls <liubushilushan@gmail.com>
2024-08-21 00:17:38 +0800

    m

---
+++ backtrader/backtrader-log.py
@@ -0,0 +1,39 @@
+import datetime # 
+import os.path # 路径管理  
+import sys # 获取当前运行脚本的路径 (in argv[0]) 
+#导入backtrader框架  
+import backtrader as bt 
+from feedstock import get_stock_history
+
+# 创建策略继承bt.Strategy 
+class TestStrategy(bt.Strategy): 
+
+    def log(self, txt, dt=None): 
+        # 记录策略的执行日志  
+        dt = dt or self.datas[0].datetime.date(0) 
+        print('%s, %s' % (dt.isoformat(), txt)) 
+
+    def __init__(self): 
+        # 保存收盘价的引用  
+        self.dataclose = self.datas[0].close 
+
+    def next(self): 
+        # 记录收盘价  
+        self.log('Close, %.2f' % self.dataclose[0]) 
+        
+# 创建Cerebro引擎  
+cerebro = bt.Cerebro() 
+# Cerebro引擎在后台创建broker(经纪人)，系统默认资金量为10000 
+# 为Cerebro引擎添加策略  
+cerebro.addstrategy(TestStrategy) 
+
+data = get_stock_history() # 加载交易数据  
+
+cerebro.adddata(data)
+# 设置投资金额100000.0 
+cerebro.broker.setcash(100000.0) 
+# 引擎运行前打印期出资金  
+print('组合期初资金: %.2f' % cerebro.broker.getvalue()) 
+cerebro.run() 
+# 引擎运行后打期末资金  
+print('组合期末资金: %.2f' % cerebro.broker.getvalue())
---
+++ backtrader/feedstock.py
@@ -0,0 +1,25 @@
+import datetime
+import pandas as pd
+import backtrader as bt
+from datetime import datetime
+import akshare as ak
+
+def get_stock_history ():
+    stock_hfq_df = ak.stock_zh_a_hist(symbol="000001", adjust="hfq").iloc[:, :6]
+        # 处理字段命名，以符合 Backtrader 的要求
+    stock_hfq_df.columns = [
+        'date',
+        'open',
+        'close',
+        'high',
+        'low',
+        'volume',
+        ]
+    # 把 date 作为日期索引，以符合 Backtrader 的要求
+    stock_hfq_df.index = pd.to_datetime(stock_hfq_df['date'])
+    start_date = datetime(2024, 1, 1)  # 回测开始时间
+    end_date = datetime(2024, 4, 15)  # 回测结束时间
+    data = bt.feeds.PandasData(dataname=stock_hfq_df, 
+                           fromdate=start_date, 
+                           todate=end_date)  # 加载数据
+    return data
